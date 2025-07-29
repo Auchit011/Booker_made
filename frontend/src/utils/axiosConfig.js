a49134737674fc8d924dd490e76b3f8fd14dd521
unknown <auchityachauhan131@gmail.com>
2025-07-29 15:27:01 +0530

    4th

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 export const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://booker-made.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
